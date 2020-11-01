7f2a34afc5d587ef94805c95a732197da853373b
Artem Zayarny <Timaz@Artems-MacBook-Pro.local>
2020-11-01 12:20:57 +0200

    Error boundary example

---
+++ error-boundary/src/components/Counter.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Counter(props) {
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        console.log('count', count)
+        if (count === 3) {
+            throw new Error()
+        }
+    }, [count])
+
+    return (
+        <>
+            <p>{count}</p>
+            <button onClick={() => { setCount(count + 1) }}>+</button>
+        </>
+    )
+}
